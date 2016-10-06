98a8bef7b2dafff71391db4f8e15d6c7a3d680c8
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2016-10-06 10:06:31 +0530

    Add files via upload

---
+++ Palindrome number.C
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+int main(void)
+ {
+
+      int num,rev=0,temp,a;
+	
+      scanf("%d",&num);
+
+      a=num;
+	
+    while(num!=0)
+	
+   {
+	   
+   temp=num%10;
+	
+   rev=temp+(rev*10);
+
+   num=num/10;
+
+   }
+	
+   if(a==rev)
+	
+     printf("The number is palindrome");
+	
+   else
+	
+   printf("The number is not a palindrome");
+	
+   return 0;
+
+}
+
