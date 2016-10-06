a601491e5391a95f8b961ddd7d6f888df8cddd34
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2016-10-06 10:13:34 +0530

    Add files via upload

---
+++ Oddnum.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main(void)
+ {
+	
+    int i,min,max;
+	
+    scanf("%d",&min);
+	
+   scanf("%d",&max);
+
+	
+     for(i=min;i<=max;i++)
+	
+       if(i%2!=0)
+	
+      printf("%d\n",i);
+	
+    return 0;
+
+}
+
