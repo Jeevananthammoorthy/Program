b2047696fdd1b9de22dd9169ec26ce179190eaff
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2016-10-06 09:42:01 +0530

    Add files via upload

---
+++ Power.C
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int main(void)
+ {
+	
+     double base,exponent,result=1,temp=0;
+     scanf("%lf",&base);
+	
+     scanf("%lf",&exponent);
+
+     temp=exponent;
+
+     while(exponent!=0)
+
+    {
+	  
+          result*=base;
+	  
+          --exponent;
+	
+   }
+	
+    printf("The power of(%.1lf and%.1lf) is%.2lf",base,temp,result);
+	
+   return 0;
+
+}
+
