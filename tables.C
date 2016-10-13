1d7b84d1f4f79b4ac12e46107ba2bf268171be8e
Jeevananthammoorthy <jeevanantham0207@gmail.com>
2016-10-13 21:28:58 +0530

    Create tables.C

---
+++ tables.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main(void)
+ {
+	
+int n,i,c;
+	
+scanf("%d",&n);
+	
+for(i=1;i<=16;i++)
+	
+{   
+ c=i*n;
+	   
+ printf("%dx%d=%d\n",i,n,c);
+	
+}
+	
+return 0;
+
+}
+
